3b6561894df7b8a48d09f9599635d71c91585eed
Андрей Едунов <a.edunov@vk.team>
2024-02-10 15:31:19 +0300

    created simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log(111);
+import "dotenv/config";
+import http from "node:http";
+
+const baseURL = "/api/users";
+
+const server = http.createServer(function (request, response) {
+  if (!request.url?.startsWith(baseURL)) {
+    response.writeHead(404);
+    response.end(JSON.stringify({ message: "URL not found", code: 404 }));
+
+    return;
+  }
+
+  console.log("Тип запроса:", request.method);
+  // console.log(request.headers);
+
+  response.end();
+});
+
+server.listen(process.env.PORT, () =>
+  console.log(`Server started at http://localost:${process.env.PORT}`)
+);
